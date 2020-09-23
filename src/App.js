c60a66613b5a433d682890216021ee592120bdd7
vasil <vasil1995.17@gmail.com>
2020-09-23 12:11:46 +0400

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+function Name (props){
+  return (
+    <div>
+      {props.name.toUpperCase()}
+    </div>
+    
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>hello</h3>
+      < Name  name= "johan"/>
+      
     </div>
   );
 }
