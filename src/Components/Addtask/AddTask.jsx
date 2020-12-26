7636e9d85f370a6c1460de74cbfa459dcdeb3516
vasil <vasil1995.17@gmail.com>
2020-12-26 17:01:37 +0400

    using hooks

--- src/Components/Addtask/AddTask.jsx
+++ src/Components/Addtask/AddTask.jsx
@@ -1,115 +1,116 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { FormControl, InputGroup, Button, Modal } from 'react-bootstrap';
 import styles from './Addtasks.module.css';
 import PropTypes from 'prop-types';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-class AddTask extends PureComponent {
-    state = {
+function AddTask(props) {
+
+    let [forms, setForms] = useState({
         title: '',
         description: '',
         date: new Date()
-    }
+        
+    });
 
-    handleChange = (event) => {
-        let {name, value} = event.target
-        this.setState({
+    let handleChange = (event) => {
+        let {name, value } = event.target; 
+        setForms({
+            ...forms,
             [name]: value
-        })
+        }) 
+        
     }
 
-    handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-            this.handleClick();
-        }
-    }
-    handleDate = (date) =>{
-       
-        this.setState({
+    let handleDate = (date) =>{
+        setForms({
+            ...forms,
             date
         })
     };
+    
+    let handleClick = () => {
 
-    handleClick = () => {
-        let { title, description, date } = this.state;
-        if (! title ) {
-            return;
+        let {title, description, date } = forms;
+        if(!title){
+            return
         }
+
         let task = {
-            title: title,
-            description: description,
-            date : date.toISOString().slice(0, 10)
-        };
-        this.props.onAdd(task);
+            title,
+            description,
+            date: date.toISOString().slice(0, 10)
+        }
+        props.onAdd(task);
     };
-
-   
-   
-
-    render() {
-        
-        return (
-            <>
-                <Modal
-                    show={true}
-                    onHide={() => this.props.onClose()}
-                    centered
-                >
-                    <Modal.Header closeButton>
-                        <Modal.Title>Add New Task</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <div>
-                            <InputGroup className={styles.input}>
-                                <FormControl
-                                    placeholder="Add Task"
-                                    name="title"
-                                    onChange={this.handleChange}
-                                    onKeyDown={this.handleKeyDown}
-                                />
-                            </InputGroup>
-                        </div>
-                        <div>
-                            <textarea 
-                            className={styles.area}
-                            name="description"
-                            onChange={this.handleChange} 
-                            rows='5'
-                            placeholder="Task Description"
-                            >
-                            </textarea>
-                        </div>
-                        <div>
-                         <DatePicker
-                          selected={this.state.date}
-                          onChange={this.handleDate}
-                          minDate={new Date()}
-                        />
-                        </div>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button
-                            variant="success"
-                            onClick={this.handleClick}
+    
+    let handleKeyDown = (event) => {
+        if(event.key ==='Enter'){
+            handleClick();
+        }
+    }
+    
+    return (
+        <>
+            <Modal
+                show={true}
+                onHide={() => props.onClose()}
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add New Task</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>
+                        <InputGroup className={styles.input}>
+                            <FormControl
+                                placeholder="Add Task"
+                                name="title"
+                                onChange={handleChange}
+                                onKeyDown={handleKeyDown}
+                            />
+                        </InputGroup>
+                    </div>
+                    <div>
+                        <textarea 
+                        className={styles.area}
+                        name="description"
+                        onChange={handleChange} 
+                        rows='5'
+                        placeholder="Task Description"
                         >
-                            Save Task
-                        </Button>
-                        <Button
-                            variant="danger"
-                            onClick={() => this.props.onClose()}
-                        >
-                            Close
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
+                        </textarea>
+                    </div>
+                    <div>
+                     <DatePicker
+                      selected={forms.date}
+                      onChange={handleDate}
+                      minDate={new Date()}
+                    />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="success"
+                        onClick={handleClick}
+                    >
+                        Save Task
+                    </Button>
+                    <Button
+                        variant="danger"
+                        onClick={() =>props.onClose()}
+                    >
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
-            </>
+        </>
 
-        );
-
+    ); 
+      
     };
-};
 
 AddTask.propTypes = {
     onAdd: PropTypes.func.isRequired,
